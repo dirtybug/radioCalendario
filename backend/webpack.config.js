05dc7dfa0d158ebf5bad369aea1e6ac6d90ff8f3
julio <dirtybug@gmail.com>
2024-11-03 19:07:22 +0000

    fix

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js', // Entry point of your application
+    entry: 'server.js.js', // Entry point of your application
     output: {
         filename: 'bundle.js', // Output filename
         path: path.resolve(__dirname, 'dist'), // Output path
