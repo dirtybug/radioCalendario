2a0dc4de6a00b171f5a0e2d1a73900defa0317a6
julio <dirtybug@gmail.com>
2024-11-03 19:02:50 +0000

    webpack

---
+++ backend/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js', // Entry point of your application
+    output: {
+        filename: 'bundle.js', // Output filename
+        path: path.resolve(__dirname, 'dist'), // Output path
+    },
+    mode: 'development', // Change to 'production' for production builds
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader', // If using Babel for transpilation
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+};
